757bc7b71f10ef0220107007aa540ee592404b20
Aman <amanueltek19@gmail.com>
2021-05-25 12:23:30 -0400

    second commit

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -19,7 +19,7 @@ const Chat = ({ location }) => {
 	const [message, setMessage] = useState('');
 	const [messages, setMessages] = useState([]);
 
-	const ENDPOINT = 'http://localhost:5000';
+	const ENDPOINT = 'https://mysocketio-chat-app.herokuapp.com/';
 
 	useEffect(() => {
 		const { name, room } = queryString.parse(location.search);
